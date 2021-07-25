e097901fe5e6059fb4219dda08c19a8327a99caa
Daniel <daniielroig@gmail.com>
2021-07-25 19:08:16 +0200

    Fix notify_service

--- notifiers/notify_service.py
+++ notifiers/notify_service.py
@@ -4,9 +4,11 @@ from notifiers.email import *
 from notifiers.telegram import *
 
 
-def notify_service(notification):
-    if os.environ.get('EMAIL_NOTIFICATION'):
-        send_email(notification)
+def notify_service(notifications):
 
-    if os.environ.get('TELEGRAM_NOTIFICATION'):
-        send_telegram(notification)
+    for notification in notifications:
+        if eval(os.environ.get('EMAIL_NOTIFICATION')):
+            send_email(notification)
+
+        if eval(os.environ.get('TELEGRAM_NOTIFICATION')):
+            send_telegram(notification)
